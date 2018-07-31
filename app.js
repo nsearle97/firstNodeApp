0debf534fac0f06cb40376b955b7c8a092d1746b
Noah Searle <noah.searle97@gmail.com>
2018-07-31 21:26:42 +0100

    ip

--- app.js
+++ app.js
@@ -87,5 +87,5 @@ app.get("/logout", function(req, res){
 // })
 
 app.listen(port, ip, function(){
-	console.log("Server has started on port " + port + "!")
+	console.log("Server has started on port " + port + " and IP + " ip + "!");
 })
