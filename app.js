a5325cf9c52df783dfa4da0c271d156c8d5fb305
Noah Searle <noah.searle97@gmail.com>
2018-07-31 21:38:57 +0100

    removed ip from app.listen

--- app.js
+++ app.js
@@ -86,7 +86,6 @@ app.get("/logout", function(req, res){
 // 	}	
 // })
 
-app.listen(port, ip, function(){
+app.listen(port, function(){
 	console.log("Server has started on port " + port + "!")
-	console.log("Server has started on IP address " + ip + "!")
 })
